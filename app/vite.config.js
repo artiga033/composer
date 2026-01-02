e5271d9a5f039fff1f61aaa48f00094f25c8916f
artiga033 <artiga033@hotmail.com>
2026-01-02 21:14:46 +0800

    chore: setup github pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import genToolsMetadata from "./build_plugins/gen_tools_metadata";
-import {} from "vite";
 
 export default defineConfig({
     plugins: [genToolsMetadata()],
